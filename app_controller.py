1b91c03ffa65030fcb49ea3d9ef9650da162f8a1
Jason clini <33248688+Psyceric@users.noreply.github.com>
2024-08-24 18:06:52 -0400

    Basic Psudo Code
    Defined basic functionality with Psudo Code, and comments.

---
+++ app_controller.py
@@ -0,0 +1,45 @@
+from tkinter import *
+from enum import Enum
+from riot_api import RiotAPI
+# Search bar
+    # Validate Username
+    # Search Username
+    # Override Username
+
+class AppController():
+    
+    class Scenes(Enum):
+        ERROR = -1
+        STARTUP = 0
+        PLAYER_STATS = 1
+
+    scene_directory = {}
+
+    class RiotSearchbar():
+        search_callback : callable
+        def __init__(self, root : Tk, callback : callable):
+            self.search_callback = callback
+            # Create GUI elements in Root frame
+            # Assign Callback function to to search_username when hitting Enter
+            pass
+
+        def search_username(self, username):
+            # If user_id passes Regex
+            # get PUUID
+            # Return PUUID w/ callback
+            pass
+
+        def override_username(self, new_name):
+            # Set text of Entry to be new value
+            pass
+
+    def __init__(self):
+        # Create multiple 'scenes' and assign them into the scene_directory dict - scene_dictionary[Scenes.Startup] = Frame
+        pass
+
+    def swap_scene(self, scene : Scenes):
+        # Set scene_directory[scene] to the top visibility layer
+        pass
+# -------------------------- API CALLS -------------------
+
+    
---
+++ riot_api.py
@@ -0,0 +1,15 @@
+class RiotAPI():
+    def __init__(self, key):
+        pass
+    def get_PUUID(self, username):
+        # RIOT API Call to get PUUID from username
+        pass
+    def get_games(self, PUUID):
+        # RIOT API Call to get dict of previous games
+        pass
+    def get_mastery(self, PUUID):
+        # RIOT API Call to get summoner Mastery
+        pass
+    def get_assets(self, character_id):
+        # RIOT API Call to get character assets from DDragon
+        pass
